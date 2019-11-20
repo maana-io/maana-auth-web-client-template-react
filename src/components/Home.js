2bb1c9c2d4e044f9a87bc0ea01cd814e329f61a0
witt3rd <witt3rd@witt3rd.com>
2019-11-19 16:20:53 -0800

    added graphql client and sample hook

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,22 @@
 import { Link } from "react-router-dom";
 import React from "react";
+import { useQuery } from "@apollo/react-hooks";
+import gql from "graphql-tag";
+
+const GET_INFO = gql`
+  query getInfo {
+    info {
+      id
+      name
+      description
+    }
+  }
+`;
 
 export function Home() {
+  const { loading, error, data } = useQuery(GET_INFO);
+  if (loading) return <p>Loading ...</p>;
+
   return (
     <div>
       <ul>
@@ -11,6 +26,7 @@ export function Home() {
       </ul>
       <div>
         <h1>Congrats, you've been authenticated!</h1>
+        <h2>{data.info.name}</h2>
       </div>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -1,22 +1,60 @@
-import "./index.css";
-
-import * as serviceWorker from "./serviceWorker";
-
-import { App, AuthContextProvider } from "./components";
-
+// --- External imports
 import React from "react";
 import ReactDOM from "react-dom";
 import { Router } from "react-router-dom";
+
+// Apollo imports
+import ApolloClient from "apollo-client";
+import { createHttpLink } from "apollo-link-http";
+import { InMemoryCache } from "apollo-cache-inmemory";
+import { ApolloProvider } from "react-apollo";
+import { setContext } from "apollo-link-context";
+
+// --- Internal imports
+import "./index.css";
+import * as serviceWorker from "./serviceWorker";
+import { App, AuthContextProvider } from "./components";
 import { getUserAuthClient } from "./util/Auth";
+import UserContext from "./util/UserContext";
 import history from "./util/history";
 
 const authClient = getUserAuthClient();
 
+// Client setup
+// - allow this service to be a client of a remote service
+//
+const uri = process.env.REACT_APP_MAANA_ENDPOINT;
+console.log("REACT_APP_MAANA_ENDPOINT", uri);
+
+const authLink = setContext((_, { headers }) => {
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      ...UserContext.getAuthHeader()
+    }
+  };
+});
+
+const httpLink = createHttpLink({ uri, fetch });
+
+// Now that subscriptions are managed through RabbitMQ, WebSocket transport is no longer needed
+// as it is not production-ready and causes both lost and duplicate events.
+const link = authLink.concat(httpLink);
+
+const client = new ApolloClient({
+  link,
+  cache: new InMemoryCache().restore(window.__APOLLO_STATE__)
+});
+
 ReactDOM.render(
   <AuthContextProvider authClient={authClient}>
-    <Router history={history}>
-      <App />
-    </Router>
+    <ApolloProvider client={client}>
+      <Router history={history}>
+        <App />
+      </Router>
+    </ApolloProvider>
+    ,
   </AuthContextProvider>,
   document.getElementById("root")
 );
